ff842e9d10af703f3b4cb288baa32a4a6b550b04
ara.214gg <aromeroa.1500@outlook.ipn.mx>
2023-11-09 22:04:02 -0600

    Aplicacion terminada

---
+++ src/pages/CarritoPage.jsx
@@ -0,0 +1,61 @@
+import { useContext } from "react"
+import { CarritoContext } from "../context/CarritoContext"
+
+export const CarritoPage = () => {
+    const {listaCompras, aumentarCantidad, disminuirCantidad, eliminarCompra} = useContext(CarritoContext);
+  
+    const calcularTotal = () =>{
+        return listaCompras.reduce((total, item) => total + item.price * item.cantidad, 0).toFixed(2);
+    }
+    
+    const handleAumentar = (id) => {
+        aumentarCantidad(id)
+    }
+    const handleReducir = (id) => {
+        disminuirCantidad(id)
+    }
+    const handleEliminar = (id) => {
+        eliminarCompra(id)
+    }
+
+    return (
+    <>
+        <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listaCompras.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.title}</td>
+                            <td>{item.price}</td>
+                            <td>
+                                <button className="btn btn-outline-primary" onClick={() => handleReducir(item.id)}>-</button>
+                                <button className="btn btn-primary">{item.cantidad}</button>
+                                <button className="btn btn-outline-primary" onClick={() => handleAumentar(item.id)}>+</button>
+
+                            </td>
+                            <td><button className="btn btn-danger" onClick={() => handleEliminar(item.id)}>Eliminar</button></td>
+                        </tr>
+                    ))}
+                    <tr>
+                        <th scope="row">TOTAL:</th>
+                        <td></td>
+                        <td>${calcularTotal()}</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+            <div className="d-grid gap-2">
+                <button className="btn btn-primary" onClick={() => print()}>COMPRAR</button>
+            </div>
+    </>
+  )
+}
